613000ad8aba63a05f6bbf0e7ba92ae804ed9b64
Leksa322 <118078120+Leksa322@users.noreply.github.com>
2023-05-03 12:38:14 +0300

    Add files via upload

---
+++ roiaka_task1.py
@@ -0,0 +1,7 @@
+print("Introduction to programming, Lab № 1")
+print("Roiaka Oleksii IKM-221a")
+TEACHER_NAME = 'Vladislav'
+
+print(f'{TEACHER_NAME}, ' * 45 + TEACHER_NAME)
+
+print(113.2/(21.2+11.3)+22.2/(15.2-11.3))
